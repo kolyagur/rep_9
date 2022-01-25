e2cec06285762daaa43a86b36d07a90165268ef7
unknown <you@example.com>
2022-01-25 16:34:55 +0300

    код связанный с путями добавлен в click

--- main9.py
+++ main9.py
@@ -2,6 +2,17 @@ import click
 import datetime
 from enum import Enum
 import random
+import logging
+from pathlib import Path
+
+logfile = 'log_1.log'
+
+log = logging.getLogger("my_log")
+log.setLevel(logging.INFO)
+FH = logging.FileHandler(logfile)
+basic_formater = logging.Formatter('%(asctime)s : [%(levelname)s] : %(message)s')
+FH.setFormatter(basic_formater)
+log.addHandler(FH)
 
 @click.group()
 def test():
@@ -26,6 +37,17 @@ def toy():
         PURPLE = "Puple Angel"
 
     click.echo(print(random.choice(list(Toys)).value))
+
+@test.command()
+def prename():
+    try:
+        for x in Path('directory/').iterdir():
+            p = x.parent / (x.stem.replace('-', '/') + '.txt')
+            p.parent.mkdir(parents=True, exist_ok=True)
+            x.rename(p)   
+        log.info("finish program")
+    except Exception as Ex:
+	    log.error(Ex)
 
 if __name__ == '__main__':
     test()
